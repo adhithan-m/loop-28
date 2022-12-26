8121e2cbed41569351e353ac7415ece0eab6cf94
adhithan-m <116782079+adhithan-m@users.noreply.github.com>
2022-12-26 20:14:01 +0530

    week or week end.

---
+++ week-weekend/js/week-or-end.js
@@ -0,0 +1,13 @@
+// weekday or weekend
+
+/*var date =prompt("Enter the date yyyy-mm-dd")
+var givenDate=new Date(date);
+var currentDay = givenDate.getDay();
+var dateIsInWeekend = (currentDay === 6) || (currentDay === 0);
+if(dateIsInWeekend==true)
+{
+   console.log("The given date "+givenDate+" is a Weekend");
+} else {
+   console.log("The given date " +givenDate+"is a not a Weekend");
+}*/
+
